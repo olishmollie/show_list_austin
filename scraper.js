d69629816f138659486076c75fc181d2a04b734c
Oliver Duncan <oliverduncan@icloud.com>
2017-03-08 14:25:09 -0600

    ShowDay objects contain array of show strings

--- scraper.js
+++ scraper.js
@@ -9,13 +9,14 @@ exports.scrape = function(callback) {
     var $ = cheerio.load(html);
 
     var dates = $('table', '.content');
+
     var datesArr = dates.map(function(index, element) {
       var date = $(this).prev().text().replace(/\[link\]/, '');
       return new ShowDay(date, $(this).text());
-    });
+    }).toArray();
 
-    console.dir(datesArr);
+    var showDays = JSON.stringify(datesArr);
 
-    return callback(null, dates.toString());
+    return callback(null, showDays);
   });
 }
