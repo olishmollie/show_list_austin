d6dc6852b1c1c16e322803cd9726e2eb3786d662
Oliver Duncan <oliverduncan@icloud.com>
2017-03-29 22:16:11 -0500

    Display today's shows in view

--- Show List Austin/Show List Austin/ViewController.swift
+++ Show List Austin/Show List Austin/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var showInfo: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getShowInfo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func getShowInfo() {
+        let url = URL(string: "http://localhost:3000/shows/today")
+        
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data, error == nil else { return }
+            do {
+                var text: String;
+                
+                let showInfoDict = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:AnyObject]
+                
+                let shows = showInfoDict["shows"] as! [String]
+                
+                text = shows.joined(separator: "\n")
+                
+                DispatchQueue.main.async() {
+                    self.showInfo.text = text
+                }
+                
+                
+            } catch {
+                print("ERROR!")
+            }
+        }
+        
+        task.resume()
     }
 
 
